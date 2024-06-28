86ef6fa50b790fd280b1c1b57f449e84db7e446c
Finn Fancher <fancherfinn@gmail.com>
2024-06-28 13:22:23 -0700

    update main.py

--- main.py
+++ main.py
@@ -3,14 +3,19 @@ def main():
     while True:
         start = input('Enter the starting letter: ')
         end = input('Enter the starting letter: ')
+        if((start.isalpha() and end.isalpha()) and (ord(start) < ord(end))):
+            break
+        else:
+            print("Input Error.")
 
     """
     ########################################
     Code Your Program here
     ########################################
     """
+    
 
-    print(*result)
+    print(result)
 
     ########################################
     # Do not delete the return statement
